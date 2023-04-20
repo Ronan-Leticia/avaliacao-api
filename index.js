94b1ddb3dfa67ac69889c2f205c9a66257b1b5e0
Ronan-Leticia <ronanvictor100@gmail.com>
2023-04-20 20:37:36 -0300

    Implementado o tratamento de exeção

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const fastify =  require('fastify')();
 
 fastify.get('/calcular-media', (req, res)=> {
-    const notaUm = Number(req.query.notaUm);
-    const notaDois = Number(req.query.notaDois);
-    const media = (notaUm + notaDois) /2;
-    const aluno = req.query.aluno;
-
-    if(media < 6){
-        res.send({"mensagem" : `O aluno ${aluno} obteve média ${media} por isso foi reprovado`});
-    }else if(media >= 6){
-        res.send({"mensagem": `O aluno ${aluno} obteve média ${media} por isso foi aprovado`});
-    }else{
-        res.send("É necessário colocar o nome do aluno, nota um e a nota dois");
+    try {
+        const notaUm = Number(req.query.notaUm);
+        const notaDois = Number(req.query.notaDois);
+        const media = (notaUm + notaDois) /2;
+        const aluno = req.query.aluno;
+    
+        if(isNaN(notaUm) || isNaN(notaDois) || !aluno){
+            throw "É necessário colocar o nome do aluno, nota um e a nota dois";
+        }
+        if(media < 6){
+            res.send({"mensagem" : `O aluno ${aluno} obteve média ${media} por isso foi reprovado`});
+        }else if(media >= 6){
+            res.send({"mensagem": `O aluno ${aluno} obteve média ${media} por isso foi aprovado`});
+        }
+    } catch(err){
+        res.send({"erro":err});
     }
+    
 });
 
 const PORT = 3000;
